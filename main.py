7dddf7e9210383d305e6a61a368caaf08a31c552
rafael <bboyrafinhazika@gmail.com>
2023-06-18 18:20:06 -0300

    Handle availability changes

--- main.py
+++ main.py
@@ -11,25 +11,26 @@ def syncDatabaseProducts(products: list, db: Database):
   dbProductsId = [dbProduct[0] for dbProduct in dbProducts]
   for product in products:
     if product["id"] not in dbProductsId:
-      db.insertIntoTable(TABLE_NAME, productId=product['id'], lastPrice=product['price'])
+      db.insertIntoTable(TABLE_NAME, productId=product['id'], lastPrice=product['price'], lastAvailable=1 if product['available'] else 0)
 
 def updateDatabaseProduct(product: dict, db: Database):
-    db.updateTable(tableName=TABLE_NAME, columns={"lastPrice": product['price']}, productId=product['id'])
+    db.updateTable(tableName=TABLE_NAME, columns={"lastPrice": product['price'], "lastAvailable": 1}, productId=product['id'])
   
 
 def main():
   db = Database(dbName=DB_FILE_NAME)
-  db.createTable('PRODUCTS', productId="TEXT NOT NULL", lastPrice="INT NOT NULL")
+  db.createTable(TABLE_NAME, productId="TEXT NOT NULL", lastPrice="INT NOT NULL", lastAvailable="BOOLEAN")
   products = bench.getProducts()
   sales = bench.getSales()
   
   syncDatabaseProducts(products, db)
 
   for product in products:
-    if product['reference_price'] and product['price'] < product['reference_price']:
-      lastPriceArray = db.conditionalFetchTable(TABLE_NAME, "lastPrice", productId=product['id'])
-      lastPrice = lastPriceArray[0][0]
-      if product['price'] != lastPrice:
+    if product['reference_price'] and product['price'] < product['reference_price'] and product['available']:
+      lastState = db.conditionalFetchTable(TABLE_NAME, "lastPrice", "lastAvailable", productId=product['id'])
+      lastPrice = lastState[0][0]
+      lastAvailable = lastState[0][1]
+      if product['price'] != lastPrice or lastAvailable == False:
         print(f"{product['title']}\n{lastPrice} -> {product['price']} < {product['reference_price']}")
         telegram.sendPhoto(product['image_url'], MessageFormatter(product, sales))
         updateDatabaseProduct(product, db)

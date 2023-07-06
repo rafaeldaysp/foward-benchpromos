2bdede53764477e9169ba3eee01cd2d159a4fdfd
rafael <bboyrafinhazika@gmail.com>
2023-07-06 13:55:15 -0300

    new features

--- utils/messageFormatter.py
+++ utils/messageFormatter.py
@@ -4,15 +4,15 @@ def MessageFormatter(product: dict, sales: list):
   if product['cashback']: priceWithoutCashback = 'R$ {:,.2f}'.format(product['price']/(100-product['cashback']['value'])).replace('.', '-').replace(',', '.').replace('-', ',')
   for sale in sales:
     if sale["product_id"] and sale["product_id"] == product['id']: 
-      specsFromSale = '🔴' + sale["specs"].replace('🔴', '') + '🔴\n\n'
+      specsFromSale = '🔴 ' + sale["specs"].replace('🔴', '') + ' 🔴\n\n'
       break
   price = 'R$ {:,.2f}'.format(product['price']/100).replace('.', '-').replace(',', '.').replace('-', ',')
   finalPrice = price
   if priceWithoutCashback: finalPrice =  priceWithoutCashback + ' (Sem Cashback)\n💰 ' + price + ' (Com Cashback)'
-  coupon = f'\n🎟 Cupom: {product["coupon"]["code"]}\n' if product['coupon'] else ''
+  coupon = f'🎟 Cupom: {product["coupon"]["code"]}\n' if product['coupon'] else ''
   link = f'https://benchpromos.com/produto/{product["id"]}'
   cashback = f'🟢 Tem {product["cashback"]["value"]}% de Cashback usando o {product["cashback"]["name"]}, se você não utiliza, entra aqui >\
   {product["cashback"]["affiliatedLink"]} 🟢' if product['cashback'] else ''
-  telegramMessage = f"🔥 {product['title']} - {price} 🔥\n\n {specsFromSale if specsFromSale else ''} {coupon}💸 {finalPrice}\n\n🔗 {link}\n\n{cashback}"
+  telegramMessage = f"🔥 {product['title']} - {price} 🔥\n\n{specsFromSale if specsFromSale else ''} {coupon}💸 {finalPrice}\n\n🔗 {link}\n\n{cashback}"
 
   return telegramMessage
